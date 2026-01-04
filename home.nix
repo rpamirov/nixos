f8ea7da48eac655781b21f3a93c26f2ddd0dcbbb
Ruslan Amirov <ruslan.amirov@pm.me>
2026-01-04 13:55:57 +0300

    zsh and starship

--- home.nix
+++ home.nix
@@ -30,12 +30,76 @@
     EDITOR = "nvim";
   };
   programs.home-manager.enable = true;
+  #SHELL
+  programs.starship.enable = true;
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    initContent = ''
+      eval "$(starship init zsh)"
+      # Ignore heavy / useless dirs for completion
+      zstyle ':completion:*' ignored-patterns \
+        '.git' \
+        '.direnv' \
+        '.cache' \
+        '.local' \
+        '.nix-profile' \
+        '.nix-defexpr' \
+        '.nix-channels' \
+        'node_modules' \
+        'result' \
+        'dist' \
+        'build'
+      # FZF: ignore nix + system trash
+      export FZF_DEFAULT_COMMAND="
+        fd --type f --hidden --follow \
+        --exclude .git \
+        --exclude .cache \
+        --exclude .local \
+        --exclude .nix-profile \
+        --exclude .nix-defexpr \
+        --exclude .nix-channels \
+        --exclude node_modules \
+        --exclude result
+      "
+      export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+      export FZF_ALT_C_COMMAND="fd --type d --hidden --follow \
+        --exclude .git \
+        --exclude .cache \
+        --exclude .local \
+        --exclude .nix-profile \
+        --exclude .nix-defexpr \
+        --exclude .nix-channels \
+        --exclude node_modules \
+        --exclude result"
+      export FZF_DEFAULT_OPTS="
+        --preview 'bat --style=numbers --color=always --line-range :300 {}'
+        --preview-window=right:60%:wrap
+      "
+        '';
+    history = {
+      size = 10000;
+      save = 10000;
+      path = "$HOME/.zsh_history";
+      ignoreDups = true;
+      ignoreAllDups = true;
+      ignoreSpace = true;
+      expireDuplicatesFirst = true;
+      share = true;
+    };
+    plugins = [
+      {
+        name = "zsh-vi-mode";
+        src = pkgs.zsh-vi-mode;
+      }
+  ];
     shellAliases = {
-    btw = "echo nixos by the way";
+#      btw = "echo nixos by the way";
     };
   };
+
   programs.git = {
     enable = true;
     settings = {
@@ -47,6 +111,15 @@
       };
   };
 
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+  
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
   gtk = {
     enable = true;
     theme = {
@@ -70,4 +143,6 @@
   xdg.configFile."hypr".source = ./dotfiles/hypr;
   xdg.configFile."kitty".source = ./dotfiles/kitty;
   xdg.configFile."waybar".source = ./dotfiles/waybar;
+  xdg.configFile."starship.toml".source = ./dotfiles/starship.toml;
+  xdg.configFile."rg".source = ./dotfiles/rg;
 }
