cec070c6bd0398db99fdcc2253f698af02762db9
Ruslan Amirov <ruslan.amirov@pm.me>
2026-01-08 22:23:37 +0300

    zellij theme

--- flake.nix
+++ flake.nix
@@ -3,39 +3,57 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "home-manager";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+
+    zjstatus = {
+      url = "github:dj95/zjstatus";
     };
   };
 
   outputs =
-    { nixpkgs, home-manager, nixvim, zen-browser, ... }:
+    {
+      nixpkgs,
+      home-manager,
+      nixvim,
+      zen-browser,
+      zjstatus,
+      ...
+    }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (final: prev: {
+            zjstatus = zjstatus.packages.${system}.default;
+          })
+        ];
+      };
     in
     {
-      homeConfigurations."rpamirov" = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations.rpamirov = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [
-	  nixvim.homeModules.nixvim 
+          nixvim.homeModules.nixvim
           zen-browser.homeModules.beta
-	  ./home.nix
-	];
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+          ./home.nix
+        ];
       };
     };
 }
