277eb018ad699f50f6e50ea83cb94b13930064b5
Ruslan Amirov <ruslan.amirov@pm.me>
2026-01-11 13:09:33 +0300

    remove pkgs

--- machines/common/packages-evocargo.nix
+++ machines/common/packages-evocargo.nix
@@ -5,18 +5,10 @@
     # work-specific tools
     docker
     docker-compose
-    python3
-    python311Packages.virtualenv
-    ros2
-    colcon
-    wireshark
-    tcpdump
-    can-utils
   ];
 
   programs.wireshark.enable = true;
   users.users.rpamirov.extraGroups = [
     "docker"
-    "wireshark"
   ];
 }
